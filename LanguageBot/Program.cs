8ee047b6494bf6a4b7ef5696b47b32e3c16f1db9
Artem-neloshkaPerevtilennia <bandura.artem@lll.kpi.ua>
2025-01-05 16:08:21 +0200

    try to get rid of console.readkey() again

--- LanguageBot/Program.cs
+++ LanguageBot/Program.cs
@@ -18,6 +18,8 @@ namespace LanguageBot
 			TelegramBotClient bot = new(botToken);
 
 			bot.StartReceiving(Update, Error);
+			Console.WriteLine("Bot is running...");
+    		Thread.Sleep(Timeout.Infinite);
 		}
 
 		private static Task Error(ITelegramBotClient bot, Exception exception, CancellationToken token)
