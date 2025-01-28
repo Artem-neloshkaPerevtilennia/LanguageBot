12c0b02accc8a8d22370f8309e493e8db2277a3a
Artem-neloshkaPerevtilennia <bandura.artem@lll.kpi.ua>
2025-01-28 15:47:03 +0200

    maybe this

--- LanguageBot/dbConnector.cs
+++ LanguageBot/dbConnector.cs
@@ -7,16 +7,28 @@ class dbConnector
 	private readonly string? _dbConnectionString;
 
 	public dbConnector()
-    {
-        // string? host = Utility.GetEnvironmentVariable("DB_HOST");
-        // string? port = Utility.GetEnvironmentVariable("DB_PORT");
-        // string? user = Utility.GetEnvironmentVariable("DB_USER");
-        // string? password = Utility.GetEnvironmentVariable("DB_PASSWORD");
-        // string? database = Utility.GetEnvironmentVariable("DB_NAME");
+	{
+		// string? host = Utility.GetEnvironmentVariable("DB_HOST");
+		// string? port = Utility.GetEnvironmentVariable("DB_PORT");
+		// string? user = Utility.GetEnvironmentVariable("DB_USER");
+		// string? password = Utility.GetEnvironmentVariable("DB_PASSWORD");
+		// string? database = Utility.GetEnvironmentVariable("DB_NAME");
 
-        //_dbConnectionString = $"Server={host};Port={port};Database={database};User={user};Password={password};";
-				_dbConnectionString = Utility.GetEnvironmentVariable("DB-URL");
+		//_dbConnectionString = $"Server={host};Port={port};Database={database};User={user};Password={password};";
+		string? mysqlUrl = Utility.GetEnvironmentVariable("DB_URL");
+
+		if (!string.IsNullOrEmpty(mysqlUrl))
+		{
+			var uri = new Uri(mysqlUrl);
+			string userInfo = uri.UserInfo; // login:password
+			string[] userPass = userInfo.Split(':');
+
+			_dbConnectionString = $"Server={uri.Host};Port={uri.Port};" +
+																$"Database={uri.AbsolutePath.TrimStart('/')};" +
+																$"User={userPass[0]};Password={userPass[1]};" +
+																$"SslMode=Preferred;";
 		}
+	}
 
 	public bool TestConnection()
 	{
